77cfb96e8cab628eb9e7b002c2d713a8333b7c2b
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-05-01 12:43:38 -0300

    refactor: new conditional for sorting books

--- ordenaLivros.js
+++ ordenaLivros.js
@@ -3,17 +3,14 @@ const livros = require('./listaLivros');
 let maisBarato = 0;
 let maisCaro = 0;
 
-for (let atual = 0; atual < livros.length; atual++) {
-  if (livros[atual].preco < livros[maisBarato].preco) {
-    maisBarato = atual;
-  };
-};
-
-for (let atual = 0; atual < livros.length; atual++) {
-  if (livros[atual].preco > livros[maisCaro].preco) {
-    maisCaro = atual;
-  };
-};
+for (let i = 0; i < livros.length; i++) {
+  if (livros[i].preco < livros[maisBarato].preco) {
+    maisBarato = i;
+  }
+  if (livros[i].preco > livros[maisCaro].preco) {
+    maisCaro = i;
+  }
+}
 
 console.log(`O livro mais barato custa R$${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`);
 console.log(`O livro mais caro custa R$${livros[maisCaro].preco} e o título é ${livros[maisCaro].titulo}`);
