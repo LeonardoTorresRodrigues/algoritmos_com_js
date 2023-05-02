b47ac436ed0c559042574b64cccc20949dc942c6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-05-02 12:21:17 -0300

    refactor: change for loop to forEach loop

--- selectionSort.js
+++ selectionSort.js
@@ -1,7 +1,7 @@
 const livros = require('./listaLivros');
 const menorValor = require('./menorValor');
 
-for (let i = 0; i < livros.length - 1; i++) {
+livros.forEach((_, i) => {
   let menor = menorValor(livros, i);
 
   let livroAtual = livros[i];
@@ -12,6 +12,6 @@ for (let i = 0; i < livros.length - 1; i++) {
 
   livros[i] = livroMenorPreco;
   livros[menor] = livroAtual;
-}
+})
 
 console.log(livros);
