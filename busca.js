932370a6e367864f1212709e036cfea559a6cf8d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-05-20 11:34:01 -0300

    feat: new search function

---
+++ busca.js
@@ -0,0 +1,20 @@
+const listaLivrosOrdenada = require('./arrayOrdenado');
+
+function busca(array, de, ate, valorBuscado) {
+  const meio = Math.floor((de + ate) / 2);
+  const atual = array[meio];
+
+  if (valorBuscado === atual.preco) {
+    return meio;
+  }
+
+  if (valorBuscado < atual.preco) {
+    return busca(array, de, meio - 1, valorBuscado);
+  }
+
+  if (valorBuscado > atual.preco) {
+    return busca(array, meio + 1, ate, valorBuscado);
+  }
+}
+
+console.log(busca(listaLivrosOrdenada, 0, listaLivrosOrdenada.length - 1, 60));
